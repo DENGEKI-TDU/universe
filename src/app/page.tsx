8e87ef0f020108d4327ea1a49466a03f1ccf8755
kimshun0213kr <96727208+kimshun0213kr@users.noreply.github.com>
2024-06-14 01:07:30 +0900

    Update: Linkの視認性の向上

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
       <Text bgGradient="linear(to-r,#69F0FD,#FE6FFD)" bgClip="text" as="span" fontWeight={"extrabold"}>UNI-VERSE!!</Text>計画は、東京電機大学鳩山キャンパス演劇部<Text as="span" fontWeight={"extrabold"}>演劇集団【でんげき】</Text>と、東京電機大学東京千住キャンパス演劇部<Text as="span" fontWeight={"extrabold"}>劇団カラクリ</Text>が一つになって、一つの劇空間を創る計画です。
       </Text>
       <Box border="linear(to-tr,#69F0FD,#FE6FFD) solid 3px">
-        <Text fontSize={"lg"}>今年のUNI-VERSE計画詳細は<Link href="/2024" color="white" fontWeight={"bold"} bgClip={"text"} bgGradient={"linear(to-r,#69F0FD,#FE6FFD)"}>こちら！</Link></Text>
-        <Text fontSize={"lg"}>UNI-VERSE計画の詳細は<Link href="/about" color="white" fontWeight={"bold"} bgClip={"text"} bgGradient={"linear(to-r,#FE6FFD,#69F0FD)"}>こちら！</Link></Text>
+        <Text fontSize={"lg"}>今年のUNI-VERSE計画詳細は<Link href="/2024" color="white" fontWeight={"bold"} bgClip={"text"} bgGradient={"linear(to-r,#69F0FD,#FE6FFD)"} borderBottom={"3px solid #69F0FD"}>こちら！</Link></Text>
+        <Text fontSize={"lg"}>UNI-VERSE計画の詳細は<Link href="/about" color="white" fontWeight={"bold"} bgClip={"text"} bgGradient={"linear(to-r,#FE6FFD,#69F0FD)"} borderBottom={"3px solid #FE6FFD"}>こちら！</Link></Text>
       </Box>
       <Card marginTop="20px">
         <CardHeader>

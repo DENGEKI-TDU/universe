5c9d3688c671dd171f94ae849d4cff92243560b9
kimshun0213kr <96727208+kimshun0213kr@users.noreply.github.com>
2024-06-19 01:27:27 +0900

    Update: フッターを作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react"
-import { Box, Center, ChakraProvider, Link } from "@chakra-ui/react";
+import { Box, Center, ChakraProvider, HStack, Link, VStack } from "@chakra-ui/react";
 import {Image} from "@chakra-ui/react"
 import { Analytics } from "@vercel/analytics/react"
 import {BIZ_UDGothic} from "next/font/google"
+import { Text } from "@chakra-ui/react";
 
 const BIZ_UDGothicFont = BIZ_UDGothic({
   weight: "400",
@@ -43,8 +44,8 @@ export default function RootLayout({
             content="https://universe.dengeki-fox.net/logo.png"
           />
         </head>
-        <body className={BIZ_UDGothicFont.className}>
         <ChakraProvider>
+        <body className={BIZ_UDGothicFont.className}>
           <Link href="/">
           <Image src="header.png" width={"100%"} />
           </Link>
@@ -53,9 +54,66 @@ export default function RootLayout({
           {children}
           </Box>
           </Center>
-          </ChakraProvider>
           <Analytics />
         </body>
+        <footer>
+          <Box bgGradient={"linear(to-r,#69F0FD,#FE6FFD)"} width={"100%"}>
+              <Center width="100%" height="100%">
+                <VStack width="100%">
+                  <Box width="100%" marginTop="15px">
+                    <Center>
+                      ©でんげき×カラクリUNI-VERSE!!実行委員会
+                    </Center>
+                  </Box>
+                  <Box className="mainSentence" width="100%">
+                    <HStack width="100%">
+                      <Center width={"100%"}>
+                      <Box width="40%" height="100%" margin={"5px"} borderRadius={"lg"} backgroundColor={"rgba(255,255,255,0.5)"}>
+                        <VStack>
+                          <Text>演劇集団でんげき</Text>
+                            <Text>X:<Link href="https://x.com/TDUdramaclub">@TDUdramaclub</Link> <br/>
+                            Site:<Link href="https://dengeki-fox.net">https://dengeki-fox.net</Link>
+                          </Text>
+                        </VStack>
+                      </Box>
+                      <Box width="40%" height="100%" margin={"5px"} borderRadius={"lg"} backgroundColor={"rgba(255,255,255,0.5)"}>
+                        <VStack>
+                          <Text>劇団カラクリ</Text>
+                          <Text>
+                            X:<Link href="https://x.com/tdu100ju_pd">@tdu1000ju_pd</Link> <br/>
+                            Instagram:<Link href="https://www.instagram.com/karakuri_tdu/">@karakuri_tdu</Link>
+                          </Text>
+                        </VStack>
+                      </Box>
+                      </Center>
+                    </HStack>
+                  </Box>
+                  <Box className="mainPhoneSentence" width="100%">
+                    <VStack width="100%">
+                      <Box width="90%" height="100%" margin={"5px"} borderRadius={"lg"} backgroundColor={"rgba(255,255,255,0.5)"}>
+                        <VStack>
+                          <Text>演劇集団でんげき</Text>
+                            <Text>X:<Link href="https://x.com/TDUdramaclub">@TDUdramaclub</Link> <br/>
+                            Site:<Link href="https://dengeki-fox.net">https://dengeki-fox.net</Link>
+                          </Text>
+                        </VStack>
+                      </Box>
+                      <Box width="90%" height="100%" margin={"5px"} borderRadius={"lg"} backgroundColor={"rgba(255,255,255,0.5)"}>
+                        <VStack>
+                          <Text>劇団カラクリ</Text>
+                          <Text>
+                            X:<Link href="https://x.com/tdu100ju_pd">@tdu1000ju_pd</Link> <br/>
+                            Instagram:<Link href="https://www.instagram.com/karakuri_tdu/">@karakuri_tdu</Link>
+                          </Text>
+                        </VStack>
+                      </Box>
+                    </VStack>
+                  </Box>
+                </VStack>
+              </Center>
+          </Box>
+        </footer>
+        </ChakraProvider>
       </html>
   );
 }
